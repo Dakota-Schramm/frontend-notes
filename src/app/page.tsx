6aebf95dc76ddb2d2ad2d2b84b71fbc7f8a2d1a1
Dakota Schramm <william.schramm11@gmail.com>
2023-09-15 11:00:44 -0500

    Add sizing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <h1 className="text-4xl">Frontend Notes</h1>
     <section className="flex flex-col items-center">
+      <h2>General</h2>
+      <p>
+        <Link href="/sizing">Sizing</Link>
+      </p>
+    </section>
+    <section className="flex flex-col items-center">
       <h2 className="text-lg">Snippets</h2>
       <ul className="text-base">
         <li className="underline">
