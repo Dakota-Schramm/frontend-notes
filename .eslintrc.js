22a52244a02158e3be67a75e27637db08b8ef708
Dakota Schramm <william.schramm11@gmail.com>
2023-09-17 16:25:26 -0500

    Created default eslint

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'standard',
+    'plugin:jsx-a11y/strict',
+    'next/core-web-vitals'
+  ],
+  rules: {
+    'max-depth': ['warn', 4],
+
+    // enabled
+    'no-bitwise': 'error',
+    'guard-for-in': 'error',
+    'no-new-func': 'error',
+    'no-undef': 'error',
+
+    'linebreak-style': ['error', 'unix'],
+
+    'no-unreachable': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-plusplus': ['warn', { allowForLoopAfterthoughts: true }],
+
+    // TODO: fix these
+    'no-unused-vars': ['warn', { args: 'none' }],
+    'no-use-before-define': ['warn', 'nofunc'],
+
+    'multiline-ternary': ['warn', 'always-multiline'],
+
+    // maybe fix these?
+    strict: 'off',
+
+    // standard-js overrides
+    'comma-dangle': 'off', // never fix this; IE 8.0 is obsolete/unsupported
+    semi: 'off', // maybe someday switch to no-semi
+    camelcase: 0,
+    'key-spacing': ['error', { mode: 'minimum' }],
+    'object-curly-newline': 0,
+    'no-multi-spaces': 0,
+    'space-in-parens': 0,
+    'no-multi-str': 1,
+    'jsx-quotes': 'error',
+
+    // jsx-a11y
+    'jsx-a11y/click-events-have-key-events': 'warn',
+    'jsx-a11y/no-static-element-interactions': 'warn',
+    'jsx-a11y/anchor-is-valid': 'warn',
+    'jsx-a11y/anchor-ambiguous-text': 'warn',
+    'jsx-a11y/lang': 'warn',
+    'jsx-a11y/prefer-tag-over-role': 'warn',
+    'jsx-a11y/label-has-associated-control': 'warn',
+  },
+  env: {
+    browser: true,
+  },
+};
