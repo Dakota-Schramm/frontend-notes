1a5dfb721ff4db304151eff348326ac8d7ab9992
Dakota Schramm <william.schramm11@gmail.com>
2023-09-19 14:45:04 -0500

    Add base prettier rules to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,64 @@
 module.exports = {
   root: true,
   extends: [
-    'eslint:recommended',
-    'plugin:jsx-a11y/strict',
-    'next/core-web-vitals'
+    // maybe add more presets here that use these other rules cared about?
+    "eslint:recommended",
+    "plugin:jsx-a11y/strict",
+    "next/core-web-vitals",
   ],
   rules: {
-    'max-depth': ['warn', 4],
+    // prettier rules
+    semi: "error",
+    "max-len": "error",
+    quotes: [ "error", "double", ],
+    "quote-props": [ "error", "as-needed", ],
+    "jsx-quotes": [ "error", "prefer-double", ],
+    "comma-dangle": [ "error", "always", ],
+    "object-curly-spacing": [ "error", "always", ],
+    "array-bracket-spacing": [ "error", "always", ],
+    // Bracket line
+    "arrow-parens": "error",
+    "eol-last": [ "error", "always", ],
+
+    "max-depth": [ "warn", 4, ],
 
     // enabled
-    'no-bitwise': 'error',
-    'guard-for-in': 'error',
-    'no-new-func': 'error',
-    'no-undef': 'error',
+    "no-bitwise": "error",
+    "guard-for-in": "error",
+    "no-new-func": "error",
+    "no-undef": "error",
 
-    'linebreak-style': ['error', 'unix'],
+    "linebreak-style": [ "error", "unix", ],
 
-    'no-unreachable': 'warn',
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-plusplus': ['warn', { allowForLoopAfterthoughts: true }],
+    "no-unreachable": "warn",
+    "no-console": [ "warn", { allow: [ "warn", "error", ], }, ],
+    "no-plusplus": [ "warn", { allowForLoopAfterthoughts: true, }, ],
 
     // TODO: fix these
-    'no-unused-vars': ['warn', { args: 'none' }],
-    'no-use-before-define': ['warn', 'nofunc'],
+    "no-unused-vars": [ "warn", { args: "none", }, ],
+    "no-use-before-define": [ "warn", "nofunc", ],
 
-    'multiline-ternary': ['warn', 'always-multiline'],
+    "multiline-ternary": [ "warn", "always-multiline", ],
 
     // maybe fix these?
-    strict: 'off',
+    strict: "off",
 
     // standard-js overrides
-    'comma-dangle': 'off', // never fix this; IE 8.0 is obsolete/unsupported
-    semi: 'off', // maybe someday switch to no-semi
     camelcase: 0,
-    'key-spacing': ['error', { mode: 'minimum' }],
-    'object-curly-newline': 0,
-    'no-multi-spaces': 0,
-    'space-in-parens': 0,
-    'no-multi-str': 1,
-    'jsx-quotes': 'error',
+    "key-spacing": [ "error", { mode: "minimum", }, ],
+    "object-curly-newline": 0,
+    "no-multi-spaces": 0,
+    "space-in-parens": 0,
+    "no-multi-str": 1,
 
     // jsx-a11y
-    'jsx-a11y/click-events-have-key-events': 'warn',
-    'jsx-a11y/no-static-element-interactions': 'warn',
-    'jsx-a11y/anchor-is-valid': 'warn',
-    'jsx-a11y/anchor-ambiguous-text': 'warn',
-    'jsx-a11y/lang': 'warn',
-    'jsx-a11y/prefer-tag-over-role': 'warn',
-    'jsx-a11y/label-has-associated-control': 'warn',
+    "jsx-a11y/click-events-have-key-events": "warn",
+    "jsx-a11y/no-static-element-interactions": "warn",
+    "jsx-a11y/anchor-is-valid": "warn",
+    "jsx-a11y/anchor-ambiguous-text": "warn",
+    "jsx-a11y/lang": "warn",
+    "jsx-a11y/prefer-tag-over-role": "warn",
+    "jsx-a11y/label-has-associated-control": "warn",
   },
   env: {
     browser: true,
